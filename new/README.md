167b57f8233824234021e1f8515c70126278bd8d
DanteSU <75961552+Dante-SU@users.noreply.github.com>
2023-03-20 15:34:38 +0800

    Version1.0.0

---
+++ new/README.md
@@ -0,0 +1,4 @@
+# New part
+
+* [note](1.md)
+* [code](2.md)
---
+++ upload.sh
@@ -0,0 +1,3 @@
+cd /Users/dantesu/Documents/code/gitbook
+rm -rf ../github/Dante-BO.github.io/*
+cp -r ./_book/* ../github/Dante-BO.github.io
